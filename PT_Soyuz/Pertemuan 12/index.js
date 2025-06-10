d45ef09345aa393a0e7a27e431e474aa69808ca8
Samuelhizkya <semyulhizkya@gmail.com>
2025-06-10 12:50:26 +0800

    Delete PT_Soyuz directory

--- PT_Soyuz/Pertemuan 12/index.js
+++
@@ -1,6 +0,0 @@
-let nama = document.getElementById("nama");
-let menu = document.getElementById("menu");
-
-menu.addEventListener("change", function (e) {
-  nama.innerText = `menekan: ${e.target.value}`;
-});
